504964c25972b94e91ab8671876d33d126e37368
edso-n <232025730@aluno.unb.br>
2026-06-21 13:56:25 -0300

    feat: Criação de tela de Confirmação de Email.

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -10,7 +10,7 @@ import { Login } from '../pages/Login/Login';
 import { GerenciarCampanhas } from '../pages/GerenciarCampanhas/GerenciarCampanhas';
 import { NovaCampanha } from '../pages/NovaCampanha/NovaCampanha';
 import { CampanhaAtiva } from '../pages/CampanhaAtiva/CampanhaAtiva';
-
+import { ConfirmacaoEmail } from '../pages/ConfirmacaoEmail/ConfirmacaoEmail';
 
 export function AppRoutes() {
   return (
@@ -28,6 +28,7 @@ export function AppRoutes() {
       <Route path="/nova-campanha" element={<NovaCampanha />} />
       <Route path="/campanha-ativa" element={<CampanhaAtiva />} />
       <Route path="/campanha-ativa/:id" element={<CampanhaAtiva />} />
+      <Route path="/confirmar-email" element={<ConfirmacaoEmail />} />
     </Routes>
   );
 }
